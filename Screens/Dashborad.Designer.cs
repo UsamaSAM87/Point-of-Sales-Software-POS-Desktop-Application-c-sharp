06364bda47984726029db2db54ba0fe68e23f094
Muhammad Usama <89796333+UsamaSAM87@users.noreply.github.com>
2022-06-11 10:17:10 +0500

    Add files via upload

---
+++ Screens/Dashborad.Designer.cs
@@ -0,0 +1,258 @@
+﻿
+namespace POS_App.Screens
+{
+    partial class Dashborad
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.metroButton1 = new MetroFramework.Controls.MetroButton();
+            this.metroButton2 = new MetroFramework.Controls.MetroButton();
+            this.metroButton3 = new MetroFramework.Controls.MetroButton();
+            this.metroButton4 = new MetroFramework.Controls.MetroButton();
+            this.metroButton5 = new MetroFramework.Controls.MetroButton();
+            this.metroButton6 = new MetroFramework.Controls.MetroButton();
+            this.metroButton7 = new MetroFramework.Controls.MetroButton();
+            this.metroButton8 = new MetroFramework.Controls.MetroButton();
+            this.metroButton9 = new MetroFramework.Controls.MetroButton();
+            this.metroButton10 = new MetroFramework.Controls.MetroButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // metroButton1
+            // 
+            this.metroButton1.AutoSize = true;
+            this.metroButton1.BackColor = System.Drawing.Color.DarkTurquoise;
+            this.metroButton1.FontSize = MetroFramework.MetroButtonSize.Medium;
+            this.metroButton1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.metroButton1.Location = new System.Drawing.Point(52, 76);
+            this.metroButton1.Name = "metroButton1";
+            this.metroButton1.Size = new System.Drawing.Size(161, 90);
+            this.metroButton1.TabIndex = 0;
+            this.metroButton1.Text = "Define Products";
+            this.metroButton1.UseCustomBackColor = true;
+            this.metroButton1.UseCustomForeColor = true;
+            this.metroButton1.UseSelectable = true;
+            this.metroButton1.UseStyleColors = true;
+            this.metroButton1.Click += new System.EventHandler(this.metroButton1_Click);
+            // 
+            // metroButton2
+            // 
+            this.metroButton2.AutoSize = true;
+            this.metroButton2.BackColor = System.Drawing.Color.DarkTurquoise;
+            this.metroButton2.FontSize = MetroFramework.MetroButtonSize.Medium;
+            this.metroButton2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.metroButton2.Location = new System.Drawing.Point(235, 76);
+            this.metroButton2.Name = "metroButton2";
+            this.metroButton2.Size = new System.Drawing.Size(161, 90);
+            this.metroButton2.TabIndex = 1;
+            this.metroButton2.Text = "Manage Products";
+            this.metroButton2.UseCustomBackColor = true;
+            this.metroButton2.UseCustomForeColor = true;
+            this.metroButton2.UseSelectable = true;
+            this.metroButton2.UseStyleColors = true;
+            // 
+            // metroButton3
+            // 
+            this.metroButton3.AutoSize = true;
+            this.metroButton3.BackColor = System.Drawing.Color.DarkTurquoise;
+            this.metroButton3.FontSize = MetroFramework.MetroButtonSize.Medium;
+            this.metroButton3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.metroButton3.Location = new System.Drawing.Point(420, 76);
+            this.metroButton3.Name = "metroButton3";
+            this.metroButton3.Size = new System.Drawing.Size(161, 90);
+            this.metroButton3.TabIndex = 2;
+            this.metroButton3.Text = "Orders Record";
+            this.metroButton3.UseCustomBackColor = true;
+            this.metroButton3.UseCustomForeColor = true;
+            this.metroButton3.UseSelectable = true;
+            this.metroButton3.UseStyleColors = true;
+            // 
+            // metroButton4
+            // 
+            this.metroButton4.AutoSize = true;
+            this.metroButton4.BackColor = System.Drawing.Color.DarkTurquoise;
+            this.metroButton4.FontSize = MetroFramework.MetroButtonSize.Medium;
+            this.metroButton4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.metroButton4.Location = new System.Drawing.Point(603, 76);
+            this.metroButton4.Name = "metroButton4";
+            this.metroButton4.Size = new System.Drawing.Size(161, 90);
+            this.metroButton4.TabIndex = 3;
+            this.metroButton4.Text = "Change Password";
+            this.metroButton4.UseCustomBackColor = true;
+            this.metroButton4.UseCustomForeColor = true;
+            this.metroButton4.UseSelectable = true;
+            this.metroButton4.UseStyleColors = true;
+            // 
+            // metroButton5
+            // 
+            this.metroButton5.AutoSize = true;
+            this.metroButton5.BackColor = System.Drawing.Color.DarkTurquoise;
+            this.metroButton5.FontSize = MetroFramework.MetroButtonSize.Medium;
+            this.metroButton5.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.metroButton5.Location = new System.Drawing.Point(52, 189);
+            this.metroButton5.Name = "metroButton5";
+            this.metroButton5.Size = new System.Drawing.Size(161, 72);
+            this.metroButton5.TabIndex = 4;
+            this.metroButton5.Text = "Manage Stock";
+            this.metroButton5.UseCustomBackColor = true;
+            this.metroButton5.UseCustomForeColor = true;
+            this.metroButton5.UseSelectable = true;
+            this.metroButton5.UseStyleColors = true;
+            // 
+            // metroButton6
+            // 
+            this.metroButton6.AutoSize = true;
+            this.metroButton6.BackColor = System.Drawing.Color.DarkTurquoise;
+            this.metroButton6.FontSize = MetroFramework.MetroButtonSize.Medium;
+            this.metroButton6.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.metroButton6.Location = new System.Drawing.Point(235, 189);
+            this.metroButton6.Name = "metroButton6";
+            this.metroButton6.Size = new System.Drawing.Size(161, 72);
+            this.metroButton6.TabIndex = 5;
+            this.metroButton6.Text = "Manage Categories";
+            this.metroButton6.UseCustomBackColor = true;
+            this.metroButton6.UseCustomForeColor = true;
+            this.metroButton6.UseSelectable = true;
+            this.metroButton6.UseStyleColors = true;
+            // 
+            // metroButton7
+            // 
+            this.metroButton7.AutoSize = true;
+            this.metroButton7.BackColor = System.Drawing.Color.DarkTurquoise;
+            this.metroButton7.FontSize = MetroFramework.MetroButtonSize.Medium;
+            this.metroButton7.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.metroButton7.Location = new System.Drawing.Point(420, 189);
+            this.metroButton7.Name = "metroButton7";
+            this.metroButton7.Size = new System.Drawing.Size(161, 72);
+            this.metroButton7.TabIndex = 6;
+            this.metroButton7.Text = "Print Custom Reports";
+            this.metroButton7.UseCustomBackColor = true;
+            this.metroButton7.UseCustomForeColor = true;
+            this.metroButton7.UseSelectable = true;
+            this.metroButton7.UseStyleColors = true;
+            // 
+            // metroButton8
+            // 
+            this.metroButton8.AutoSize = true;
+            this.metroButton8.BackColor = System.Drawing.Color.Red;
+            this.metroButton8.FontSize = MetroFramework.MetroButtonSize.Medium;
+            this.metroButton8.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.metroButton8.Location = new System.Drawing.Point(603, 189);
+            this.metroButton8.Name = "metroButton8";
+            this.metroButton8.Size = new System.Drawing.Size(161, 72);
+            this.metroButton8.TabIndex = 7;
+            this.metroButton8.Text = "Exit Dashboard";
+            this.metroButton8.UseCustomBackColor = true;
+            this.metroButton8.UseCustomForeColor = true;
+            this.metroButton8.UseSelectable = true;
+            this.metroButton8.UseStyleColors = true;
+            this.metroButton8.Click += new System.EventHandler(this.metroButton8_Click);
+            // 
+            // metroButton9
+            // 
+            this.metroButton9.AutoSize = true;
+            this.metroButton9.BackColor = System.Drawing.Color.DarkTurquoise;
+            this.metroButton9.FontSize = MetroFramework.MetroButtonSize.Medium;
+            this.metroButton9.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.metroButton9.Location = new System.Drawing.Point(52, 283);
+            this.metroButton9.Name = "metroButton9";
+            this.metroButton9.Size = new System.Drawing.Size(344, 94);
+            this.metroButton9.TabIndex = 8;
+            this.metroButton9.Text = "Add Sales Order";
+            this.metroButton9.UseCustomBackColor = true;
+            this.metroButton9.UseCustomForeColor = true;
+            this.metroButton9.UseSelectable = true;
+            this.metroButton9.UseStyleColors = true;
+            // 
+            // metroButton10
+            // 
+            this.metroButton10.AutoSize = true;
+            this.metroButton10.BackColor = System.Drawing.Color.DarkTurquoise;
+            this.metroButton10.FontSize = MetroFramework.MetroButtonSize.Medium;
+            this.metroButton10.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.metroButton10.Location = new System.Drawing.Point(420, 283);
+            this.metroButton10.Name = "metroButton10";
+            this.metroButton10.Size = new System.Drawing.Size(344, 94);
+            this.metroButton10.TabIndex = 9;
+            this.metroButton10.Text = "Customer Management";
+            this.metroButton10.UseCustomBackColor = true;
+            this.metroButton10.UseCustomForeColor = true;
+            this.metroButton10.UseSelectable = true;
+            this.metroButton10.UseStyleColors = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.label1.Font = new System.Drawing.Font("Yu Gothic UI", 17F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(23, 398);
+            this.label1.Name = "label1";
+            this.label1.Padding = new System.Windows.Forms.Padding(5);
+            this.label1.Size = new System.Drawing.Size(286, 72);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "SamSoft\r\nContact: +92 300 6905880";
+            // 
+            // Dashborad
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
+            this.AutoScroll = true;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(819, 492);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.metroButton10);
+            this.Controls.Add(this.metroButton9);
+            this.Controls.Add(this.metroButton8);
+            this.Controls.Add(this.metroButton7);
+            this.Controls.Add(this.metroButton6);
+            this.Controls.Add(this.metroButton5);
+            this.Controls.Add(this.metroButton4);
+            this.Controls.Add(this.metroButton3);
+            this.Controls.Add(this.metroButton2);
+            this.Controls.Add(this.metroButton1);
+            this.MinimumSize = new System.Drawing.Size(819, 492);
+            this.Name = "Dashborad";
+            this.Text = "POS | User Dashboard ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroButton metroButton1;
+        private MetroFramework.Controls.MetroButton metroButton2;
+        private MetroFramework.Controls.MetroButton metroButton3;
+        private MetroFramework.Controls.MetroButton metroButton4;
+        private MetroFramework.Controls.MetroButton metroButton5;
+        private MetroFramework.Controls.MetroButton metroButton6;
+        private MetroFramework.Controls.MetroButton metroButton7;
+        private MetroFramework.Controls.MetroButton metroButton8;
+        private MetroFramework.Controls.MetroButton metroButton9;
+        private MetroFramework.Controls.MetroButton metroButton10;
+        private System.Windows.Forms.Label label1;
+    }
+}
