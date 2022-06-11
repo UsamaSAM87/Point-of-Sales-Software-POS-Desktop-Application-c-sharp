9ec37a56321865fc7214fd49a82a5b491bf2ea49
Muhammad Usama <89796333+UsamaSAM87@users.noreply.github.com>
2022-06-11 10:17:39 +0500

    Add files via upload

---
+++ Screens/LoginForm.Designer.cs
@@ -0,0 +1,186 @@
+﻿
+namespace POS_App
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginForm));
+            this.LoginButton = new MetroFramework.Controls.MetroButton();
+            this.IDTextbox = new MetroFramework.Controls.MetroTextBox();
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel2 = new MetroFramework.Controls.MetroLabel();
+            this.passwordTextbox = new MetroFramework.Controls.MetroTextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // LoginButton
+            // 
+            this.LoginButton.BackColor = System.Drawing.Color.LimeGreen;
+            this.LoginButton.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.LoginButton.Location = new System.Drawing.Point(341, 321);
+            this.LoginButton.Name = "LoginButton";
+            this.LoginButton.Size = new System.Drawing.Size(110, 53);
+            this.LoginButton.TabIndex = 0;
+            this.LoginButton.Text = "Login";
+            this.LoginButton.UseCustomBackColor = true;
+            this.LoginButton.UseCustomForeColor = true;
+            this.LoginButton.UseSelectable = true;
+            this.LoginButton.UseStyleColors = true;
+            this.LoginButton.Click += new System.EventHandler(this.LoginButton_Click);
+            // 
+            // IDTextbox
+            // 
+            // 
+            // 
+            // 
+            this.IDTextbox.CustomButton.Image = null;
+            this.IDTextbox.CustomButton.Location = new System.Drawing.Point(173, 1);
+            this.IDTextbox.CustomButton.Name = "";
+            this.IDTextbox.CustomButton.Size = new System.Drawing.Size(33, 33);
+            this.IDTextbox.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.IDTextbox.CustomButton.TabIndex = 1;
+            this.IDTextbox.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.IDTextbox.CustomButton.UseSelectable = true;
+            this.IDTextbox.CustomButton.Visible = false;
+            this.IDTextbox.Lines = new string[0];
+            this.IDTextbox.Location = new System.Drawing.Point(341, 194);
+            this.IDTextbox.MaxLength = 32767;
+            this.IDTextbox.Multiline = true;
+            this.IDTextbox.Name = "IDTextbox";
+            this.IDTextbox.PasswordChar = '\0';
+            this.IDTextbox.PromptText = "ID";
+            this.IDTextbox.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.IDTextbox.SelectedText = "";
+            this.IDTextbox.SelectionLength = 0;
+            this.IDTextbox.SelectionStart = 0;
+            this.IDTextbox.ShortcutsEnabled = true;
+            this.IDTextbox.Size = new System.Drawing.Size(207, 35);
+            this.IDTextbox.TabIndex = 1;
+            this.IDTextbox.UseSelectable = true;
+            this.IDTextbox.WaterMark = "ID";
+            this.IDTextbox.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.IDTextbox.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.AutoSize = true;
+            this.metroLabel1.Location = new System.Drawing.Point(176, 194);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(55, 19);
+            this.metroLabel1.TabIndex = 2;
+            this.metroLabel1.Text = "Your ID:";
+            // 
+            // metroLabel2
+            // 
+            this.metroLabel2.AutoSize = true;
+            this.metroLabel2.Location = new System.Drawing.Point(176, 255);
+            this.metroLabel2.Name = "metroLabel2";
+            this.metroLabel2.Size = new System.Drawing.Size(97, 19);
+            this.metroLabel2.TabIndex = 4;
+            this.metroLabel2.Text = "Your Password:";
+            // 
+            // passwordTextbox
+            // 
+            // 
+            // 
+            // 
+            this.passwordTextbox.CustomButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.passwordTextbox.CustomButton.Image = null;
+            this.passwordTextbox.CustomButton.Location = new System.Drawing.Point(173, 1);
+            this.passwordTextbox.CustomButton.Name = "";
+            this.passwordTextbox.CustomButton.Size = new System.Drawing.Size(33, 33);
+            this.passwordTextbox.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.passwordTextbox.CustomButton.TabIndex = 1;
+            this.passwordTextbox.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.passwordTextbox.CustomButton.UseSelectable = true;
+            this.passwordTextbox.CustomButton.Visible = false;
+            this.passwordTextbox.Lines = new string[0];
+            this.passwordTextbox.Location = new System.Drawing.Point(341, 255);
+            this.passwordTextbox.MaxLength = 32767;
+            this.passwordTextbox.Name = "passwordTextbox";
+            this.passwordTextbox.PasswordChar = '●';
+            this.passwordTextbox.PromptText = "Password";
+            this.passwordTextbox.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.passwordTextbox.SelectedText = "";
+            this.passwordTextbox.SelectionLength = 0;
+            this.passwordTextbox.SelectionStart = 0;
+            this.passwordTextbox.ShortcutsEnabled = true;
+            this.passwordTextbox.Size = new System.Drawing.Size(207, 35);
+            this.passwordTextbox.TabIndex = 3;
+            this.passwordTextbox.UseSelectable = true;
+            this.passwordTextbox.UseSystemPasswordChar = true;
+            this.passwordTextbox.WaterMark = "Password";
+            this.passwordTextbox.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.passwordTextbox.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(313, 30);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(150, 150);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(755, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.metroLabel2);
+            this.Controls.Add(this.passwordTextbox);
+            this.Controls.Add(this.metroLabel1);
+            this.Controls.Add(this.IDTextbox);
+            this.Controls.Add(this.LoginButton);
+            this.MaximizeBox = false;
+            this.Name = "LoginForm";
+            this.Resizable = false;
+            this.Text = "POS Login";
+            this.TopMost = true;
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroButton LoginButton;
+        private MetroFramework.Controls.MetroTextBox IDTextbox;
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+        private MetroFramework.Controls.MetroLabel metroLabel2;
+        private MetroFramework.Controls.MetroTextBox passwordTextbox;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Screens/ProductDefination.Designer.cs
@@ -0,0 +1,205 @@
+﻿
+namespace POS_App.Screens
+{
+    partial class ProductDefination
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(212, 77);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(198, 39);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(212, 143);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(198, 39);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(212, 210);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(198, 39);
+            this.textBox3.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(212, 340);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(198, 39);
+            this.textBox4.TabIndex = 3;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(212, 277);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(198, 39);
+            this.textBox5.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Yu Gothic", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(26, 77);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(144, 26);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Product Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Yu Gothic", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(26, 143);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 26);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Category";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Yu Gothic", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(26, 210);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(149, 26);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Supplier Name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Yu Gothic", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(26, 277);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(106, 26);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Cost Price";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Yu Gothic", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(26, 340);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(104, 26);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Sale Price";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Red;
+            this.button1.Font = new System.Drawing.Font("Yu Gothic", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(585, 338);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 41);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Close";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.DodgerBlue;
+            this.button2.Font = new System.Drawing.Font("Yu Gothic", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(467, 338);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(101, 41);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Save";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // ProductDefination
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 35F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(715, 406);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Font = new System.Drawing.Font("Yu Gothic", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(7, 9, 7, 9);
+            this.MaximizeBox = false;
+            this.Name = "ProductDefination";
+            this.Padding = new System.Windows.Forms.Padding(53, 161, 53, 54);
+            this.Resizable = false;
+            this.ShowInTaskbar = false;
+            this.Text = "Add Products";
+            this.Load += new System.EventHandler(this.ProductDefination_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
