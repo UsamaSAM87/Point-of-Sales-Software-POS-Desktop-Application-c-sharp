4323c230eaef1dcbee5e3460573d33a32ebdf367
Muhammad Usama <89796333+UsamaSAM87@users.noreply.github.com>
2022-06-11 10:15:31 +0500

    Add files via upload

---
+++ General/app_settings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.SqlClient;
+using POS_App.General;
+
+namespace POS_App.General
+{
+    public class App_settings
+    { 
+        public static string ConnectionString()
+        {
+            return ConfigurationManager.ConnectionStrings["pos_db"].ConnectionString;
+        }
+    }
+}
